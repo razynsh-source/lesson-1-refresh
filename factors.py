a68fa3ddb9ae1e98624ed5a37786baed04e20dc3
razynsh-source <razynsh@ac.sce.ac.il>
2026-05-07 11:32:01 +0300

    Update factors.py

--- factors.py
+++ factors.py
@@ -1,9 +1,7 @@
 def factors(x):
- def factors(x):
     result = []
     # עוברים על כל המספרים מ-1 ועד x כולל
     for i in range(1, x + 1):
         if x % i == 0:
             result.append(i)
     return result
-  return a_list
